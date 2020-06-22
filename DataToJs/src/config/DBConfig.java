1a22f716a2241cbc5b20aa32d4dceea6f217d6ed
parksuoh <sooho0163@naver.com>
2020-06-22 17:40:12 +0900

    aa

---
+++ DataToJs/src/config/DBConfig.java
@@ -0,0 +1,30 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConfig {
+
+	// DB정보 - 배포용
+	//public static final String HOST = "jdbc:mysql://chhak.kr:3306/pso";
+	//public static final String USER = "pso";
+	//public static final String PASS = "0924";
+	
+	// DB정보 - 개발용
+	public static final String HOST = "jdbc:mysql://192.168.44.46:3306/pso";
+	public static final String USER = "pso";
+	public static final String PASS = "1234";
+	
+	public static Connection getConnection() throws Exception {
+		// 1단계
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		// 2단계
+		Connection conn = DriverManager.getConnection(HOST, USER, PASS);
+		
+		return conn;
+	}	
+}
+
+
